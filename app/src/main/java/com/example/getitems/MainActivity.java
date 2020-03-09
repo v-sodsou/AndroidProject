8b60686acff612294b2768b2724497eba4ebd7b4
sonali dsouza <sonali.18feb@gmail.com>
2020-03-08 21:34:26 -0700

    set the click listener

--- app/src/main/java/com/example/getitems/MainActivity.java
+++ app/src/main/java/com/example/getitems/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.getitems;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               //Todo
+            }
+        });
     }
 }
