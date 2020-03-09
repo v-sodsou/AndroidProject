c710a883be40c04485a13c7983d01450e7529795
sonali dsouza <sonali.18feb@gmail.com>
2020-03-08 23:39:25 -0700

    set title to item name in ItemModelDetailFragment file

--- app/src/main/java/com/example/getitems/ItemModelDetailFragment.java
+++ app/src/main/java/com/example/getitems/ItemModelDetailFragment.java
@@ -51,9 +51,9 @@ public class ItemModelDetailFragment extends Fragment {
 
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-//            if (appBarLayout != null) {
-//                appBarLayout.setTitle(mItem.content);
-//            }
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.Name);
+            }
         }
     }
 
